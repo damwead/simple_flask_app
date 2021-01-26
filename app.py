556949d34d584b762a1a60e4841060fb71bff5e8
Innokenty Mikhalevsky <innokentymikhalevsky@Star-Wonderer.local>
2021-01-26 21:18:05 +0300

    connected with db

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -8,10 +9,15 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False) # False - we dont want it empty
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        # each time smth created returns taks and id
+        return '<Task %r>' % self.id
 
 
-
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template("index.html")
 
